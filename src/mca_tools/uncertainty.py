316836e51e7a8685ae3989a95e1ca909ead7039f
Diego Couto <stoneboyriver@gmail.com>
2026-04-01 16:32:53 +0200

    Added print_uncertainty() function Added a function to print propertly rounded values with its uncertainty in scientific notation

--- src/mca_tools/uncertainty.py
+++ src/mca_tools/uncertainty.py
@@ -7,4 +7,50 @@ def round_uncertainty(x):
     else:
         return 0
 
+def print_uncertainty(value, uncertainty):
+
+    non_decimal = str(value).split(".")[0] # Non decimal part of the value (string)
+
+    # Position of the most siginficant figure of the value and the uncertainty
+    uncert_signif_fig = int(np.floor(np.log10(abs(uncertainty))))
+    value_signif_fig = int(np.floor(np.log10(abs(value))))
+
+    # The k
+    if uncert_signif_fig > value_signif_fig:
+        raise ValueError("The uncertainty is bigger than the value!")
+
+    # Position of the 2 most significant figures of the uncertainty
+    uncert_round_fig = 1 - uncert_signif_fig
+
+    # Getting the significant figures in the string
+    # Getting the value this way makes the formatting round the uncertainty
+    # automaticaly
+    uncert_str = f"{uncertainty:.1e}"
+
+
+    # If the uncertainty has the same significant figures as
+    # its value, the uncertainty has to be written differently
+    # This is because we are using scientific notation
+    if value_signif_fig == uncert_signif_fig:
+        uncert_str = f"({uncert_str[0]}.{uncert_str[2]})"
+
+    else:
+        uncert_str = f"({uncert_str[0]}{uncert_str[2]})"
+
+    # The last check we need is if the  number of significant values on the
+    # uncertainty is larger than the main value, that needs to raise an error
+
+    # The first number doesn't count, so we have to subtract 1
+    if int(non_decimal) == 0:
+        significant_values = len(non_decimal) + uncert_round_fig - 2
+
+    else:
+        significant_values = len(non_decimal) + uncert_round_fig - 1
+
+    string_formatter = f".{significant_values}e"
+    rounded_value = f"{value:{string_formatter}}"
+
+    # We add the uncertainty in parenthesis in betweeen the rounded value
+    splitted_rounded_value = rounded_value.split("e")
+    return splitted_rounded_value[0] + uncert_str + "e" + splitted_rounded_value[1]
 
